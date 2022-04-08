6f942a32116b5f376e189f2015deac1219b893f8
legend620 <gex6611@gmail.com>
2022-04-08 10:55:47 -0400

    fix: Sort orders by date with pagination

--- src/views/Orders/components/OrderHistoryPanel.tsx
+++ src/views/Orders/components/OrderHistoryPanel.tsx
@@ -1,6 +1,7 @@
-import { OrderStatus } from '@autonomylabs/limit-stop-orders'
+import { OrderStatus, Order } from '@autonomylabs/limit-stop-orders'
 import { getAddress } from '@ethersproject/address'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import ReactPaginate from 'react-paginate'
 import { useQuery } from 'react-query'
 import styled from 'styled-components'
 import { useMatchBreakpoints, Tabs, Tab } from '@apeswapfinance/uikit'
@@ -32,13 +33,79 @@ const TabContent = styled('div')`
   border-radius: 20px;
 `
 
+const ORDERS_PER_PAGE = 5
+
+const Pagination = styled(ReactPaginate).attrs({
+  activeClassName: 'active',
+})`
+  display: flex;
+  flex-direction: row;
+  list-style-type: none;
+  padding: 0.75rem 0;
+  li {
+    height: 32px;
+    width: 32px;
+    border-radius: 7px;
+    border: gray 1px solid;
+    cursor: pointer;
+    margin-right: 0.5rem;
+  }
+  li.previous,
+  li.next,
+  li.break {
+    border-color: transparent;
+  }
+  li.active {
+    background-color: #ffb300;
+    border-color: transparent;
+    color: white;
+  }
+  li.disabled a {
+    color: grey;
+  }
+  li.disable,
+  li.disabled a {
+    cursor: default;
+  }
+
+  li a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+  }
+`
+
+const OrderRows = ({ orders }: { orders: Order[] }) => {
+  const allTokens = useAllTokens()
+
+  return (
+    <>
+      {orders.map((order) => (
+        <OrderRow
+          key={order.id}
+          order={order}
+          tokenPair={{
+            input: allTokens[getAddress(order.inputToken)],
+            output: allTokens[getAddress(order.outputToken)],
+          }}
+        />
+      ))}
+    </>
+  )
+}
+
 export default function OrderHistoryPanel() {
   const [activeTab, setActiveTab] = useState(0)
+  const [orders, setOrders] = useState<Order[]>([])
+  const [currentOrders, setCurrentOrders] = useState<Order[]>([])
+  const [pageCount, setPageCount] = useState(0)
+  const [orderOffset, setOrderOffset] = useState(0)
   const orderStatus: OrderStatus[] = ['open', 'executed', 'cancelled']
   const { isMd, isSm, isXs } = useMatchBreakpoints()
   const isMobile = isMd || isSm || isXs
 
-  const allTokens = useAllTokens()
   const autonomyOrdersLib = useAutonomyOrdersLib()
   const { account } = useActiveWeb3React()
 
@@ -50,13 +117,32 @@ export default function OrderHistoryPanel() {
       if (!account || !autonomyOrdersLib) {
         return []
       }
-      const orders = await autonomyOrdersLib.getOrders(account)
-      return orders
+      const allOrders = await autonomyOrdersLib.getOrders(account)
+      allOrders.sort((x, y) => Date.parse(y.time) - Date.parse(x.time))
+      return allOrders
     },
     {
       refetchInterval: 4_000,
     },
   )
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * ORDERS_PER_PAGE) % orders.length
+    setOrderOffset(newOffset)
+  }
+
+  useEffect(() => {
+    if (data) {
+      setOrders(data.filter((order) => order.status === orderStatus[activeTab]))
+      setOrderOffset(0)
+    }
+  }, [data, activeTab])
+
+  useEffect(() => {
+    const endOffset = orderOffset + ORDERS_PER_PAGE
+    setCurrentOrders(orders.slice(orderOffset, endOffset))
+    setPageCount(Math.ceil(orders.length / ORDERS_PER_PAGE))
+  }, [orders, orderOffset])
 
   return (
     <OrderHistoryContainer>
@@ -91,19 +177,16 @@ export default function OrderHistoryPanel() {
             </Tabs>
           </TabWrapper>
           <TabContent id="order-history-panel">
-            {data &&
-              data
-                .filter((order) => order.status === orderStatus[activeTab])
-                .map((order) => (
-                  <OrderRow
-                    key={order.id}
-                    order={order}
-                    tokenPair={{
-                      input: allTokens[getAddress(order.inputToken)],
-                      output: allTokens[getAddress(order.outputToken)],
-                    }}
-                  />
-                ))}
+            <div>
+              <OrderRows orders={currentOrders} />
+            </div>
+            <Pagination
+              previousLabel="<"
+              nextLabel=">"
+              pageCount={pageCount}
+              renderOnZeroPageCount={null}
+              onPageChange={handlePageClick}
+            />
           </TabContent>
         </AppBody>
       </StyledInputCurrencyWrapper>
